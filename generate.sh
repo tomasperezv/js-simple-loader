2dbe3d1f7fb36369308d5fa996185e9c434687a9
Tomas Perez <tom@0x101.com>
2012-06-10 00:17:57 +0400

    Adding autogenerated documentation.

---
+++ generate.sh
@@ -0,0 +1,3 @@
+cd ../jsdoc_toolkit-2.4.0/
+java -jar jsrun.jar app/run.js -a -t=templates/github-project -d=../js-simple-loader/ -projectTitle="js-simple-loader" -projectLink="http://github.com/tomasperezv/js-simple-loader" ../../js-simple-loader/loader.js
+cd -
